350b6040f0e8c8eb693921bd458c26fa0ce822d7
Antony Holmes <antony.b.holmes@gmail.com>
2017-09-29 19:37:28 -0400

    Bug fixes.

--- edu.columbia.rdf.matcalc.bio.app/src/main/java/edu/columbia/rdf/matcalc/bio/app/BioAppModuleLoader.java
+++ edu.columbia.rdf.matcalc.bio.app/src/main/java/edu/columbia/rdf/matcalc/bio/app/BioAppModuleLoader.java
@@ -5,22 +5,21 @@ import org.matcalc.toolbox.bio.genes.GenesModule;
 import org.matcalc.toolbox.bio.genes.annotation.GeneAnnotationModule;
 import org.matcalc.toolbox.bio.genes.info.GeneInfoModule;
 import org.matcalc.toolbox.bio.motifs.MotifsModule;
-import org.matcalc.toolbox.bio.pathway.PathwayModule;
-import org.matcalc.toolbox.bio.patterndiscovery.PatternDiscoveryModule;
-import org.matcalc.toolbox.bio.patterndiscovery.classifier.ClassifierModule;
 import org.matcalc.toolbox.bio.probes.ProbesModule;
-import org.matcalc.toolbox.bio.regions.RegionsModule;
 import org.matcalc.toolbox.paste.PasteModule;
 
 import edu.columbia.rdf.matcalc.bio.BioModuleLoader;
-import edu.columbia.rdf.matcalc.bio.toolbox.dna.FastaIOModule;
 import edu.columbia.rdf.matcalc.bio.toolbox.expression.ExpressionModule;
 import edu.columbia.rdf.matcalc.bio.toolbox.fillgaps.FillGapsModule;
 import edu.columbia.rdf.matcalc.bio.toolbox.probes.CollapseModule;
 import edu.columbia.rdf.matcalc.bio.toolbox.probes.ProbeLocationsModule;
 import edu.columbia.rdf.matcalc.toolbox.bio.annotation.AnnotationModule;
-import edu.columbia.rdf.matcalc.toolbox.bio.ideogram.IdeogramModule;
-import edu.columbia.rdf.matcalc.toolbox.bio.newdendrogram.NewDendrogramModule;
+import edu.columbia.rdf.matcalc.toolbox.ideogram.IdeogramModule;
+import edu.columbia.rdf.matcalc.toolbox.newdendrogram.NewDendrogramModule;
+import edu.columbia.rdf.matcalc.toolbox.pathway.PathwayModule;
+import edu.columbia.rdf.matcalc.toolbox.patterndiscovery.PatternDiscoveryModule;
+import edu.columbia.rdf.matcalc.toolbox.patterndiscovery.classifier.ClassifierModule;
+import edu.columbia.rdf.matcalc.toolbox.regions.RegionsModule;
 
 
 
